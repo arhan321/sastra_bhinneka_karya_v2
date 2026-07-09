8716a9d651b50cda838f132eb1e14f7827c1ee51
Sastra Bhinneka Auto Backup <arhanmali96@gmail.com>
2026-07-09 00:34:11 +0000

    auto commit sastra bhinneka v2: 2026-07-09 00:34:11

--- backup_database/src/uploads/backup/backup_sdk_db_sdk_db_2026-07-09_07-34-01.sql
+++ backup_database/src/uploads/backup/backup_sdk_db_sdk_db_2026-07-09_07-34-01.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------
 -- AUTO DATABASE BACKUP
 -- Database : sdk_db
--- Dibuat   : 2026-07-09 07:22:02
+-- Dibuat   : 2026-07-09 07:34:01
 -- --------------------------------------------------
 
 SET NAMES utf8mb4;
@@ -730,6 +730,7 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 ('IOjjKPlVrhXxMeGJCLaSvKwtknINKQSykKVKMccs', NULL, '10.200.7.1', 'Mozilla/5.0 (compatible)', 'eyJfdG9rZW4iOiJzcmFZR1diWVFVaHg4OWxUUW10N1Vvamo5VnZnR3pnakc0eEpWNW9TIiwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHBzOlwvXC9zYXN0cmFiaGlubmVrYWthcnlhLmNvLmlkXC9ibG9nIiwicm91dGUiOiJibG9nIn0sIl9mbGFzaCI6eyJvbGQiOltdLCJuZXciOltdfX0=', 1783549939),
 ('isqkVjRVkho1XPpNkWcDhRb6mSxYa05gszU6vk5X', NULL, '10.200.7.1', 'Mozilla/5.0 (compatible)', 'eyJfdG9rZW4iOiJYWlBhZU4zSmtyUmJOSnN5M0JwcVYzTmNFa1AyRTNLczhOVm1kZ3l5IiwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHBzOlwvXC9zYXN0cmFiaGlubmVrYWthcnlhLmNvLmlkXC9ibG9nIiwicm91dGUiOiJibG9nIn0sIl9mbGFzaCI6eyJvbGQiOltdLCJuZXciOltdfX0=', 1783549054),
 ('JQsNzs8SqdaJcmoLTMvIhhB8EcF2BOf3Jt9cknVN', NULL, '10.200.7.1', 'Mozilla/5.0 (compatible)', 'eyJfdG9rZW4iOiJ5ZXc1MzRLbFBEMmxWT2dUbGFVT2w2bzY3SzJYeDhETWp6N2JRYlhmIiwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHBzOlwvXC9zYXN0cmFiaGlubmVrYWthcnlhLmNvLmlkXC9ibG9nIiwicm91dGUiOiJibG9nIn0sIl9mbGFzaCI6eyJvbGQiOltdLCJuZXciOltdfX0=', 1783549298),
+('K1Xr0TGpPh4BtLB7Tc9RNsZxgpZUMQ3UTbOpkySr', NULL, '10.200.7.1', 'Mozilla/5.0 (compatible; AhrefsBot/7.0; +http://ahrefs.com/robot/)', 'eyJfdG9rZW4iOiJEQ1ZtMVpjTjE1Q0c4STRadjVaeXBnMWpmalVUUTQyMlhFOUt3ZElYIiwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHBzOlwvXC9zYXN0cmFiaGlubmVrYWthcnlhLmNvLmlkXC9ibG9nXC9wZXJhbi1wZXJzZXR1anVhbi1saW5na3VuZ2FuLWFtZGFsLWRhbi11a2wtdXBsLWRhbGFtLXBlbWJhbmd1bmFuIiwicm91dGUiOiJibG9nLnNob3cifSwiX2ZsYXNoIjp7Im9sZCI6W10sIm5ldyI6W119fQ==', 1783557117),
 ('Luh9Ucd8T3hSTJMYIzo9omcUEW3u4EQYqMP1caaz', NULL, '10.200.7.1', 'Mozilla/5.0 (compatible)', 'eyJfdG9rZW4iOiJJSWIwUWdrWEJuOE9GNlpTMFJwcjR4ekZYSXA3Zmc1bHo2TzdFTDB1IiwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHBzOlwvXC9zYXN0cmFiaGlubmVrYWthcnlhLmNvLmlkXC9ibG9nIiwicm91dGUiOiJibG9nIn0sIl9mbGFzaCI6eyJvbGQiOltdLCJuZXciOltdfX0=', 1783551443),
 ('MT5eJlxStWt5RXGbHqBeHnIeS0pRYIfgtnkvnORO', NULL, '10.200.7.1', 'Mozilla/5.0 (compatible)', 'eyJfdG9rZW4iOiJVUEVTMHdFUjBiNHU5RFZ1NzlUa1BEeXo5TE5sNHJiWHdJdjhGREdXIiwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHBzOlwvXC9zYXN0cmFiaGlubmVrYWthcnlhLmNvLmlkXC9ibG9nIiwicm91dGUiOiJibG9nIn0sIl9mbGFzaCI6eyJvbGQiOltdLCJuZXciOltdfX0=', 1783550441),
 ('nrzScZ18XWoTCnGQbcGcuG8kUdYzl1K6PDRhyQGB', NULL, '10.200.7.1', 'Mozilla/5.0 (compatible)', 'eyJfdG9rZW4iOiJPVkNKWHdmdzZ0TlpDZDVUTlBqZlZCeUJ3MGM4STFPMFFacUZ6UkRsIiwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHBzOlwvXC9zYXN0cmFiaGlubmVrYWthcnlhLmNvLmlkXC9ibG9nIiwicm91dGUiOiJibG9nIn0sIl9mbGFzaCI6eyJvbGQiOltdLCJuZXciOltdfX0=', 1783553370),
@@ -840,7 +841,7 @@ CREATE TABLE `visitor_logs` (
   KEY `visitor_logs_ip_address_index` (`ip_address`),
   KEY `visitor_logs_path_index` (`path`(768)),
   CONSTRAINT `visitor_logs_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8533 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8534 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Records of visitor_logs
@@ -9461,6 +9462,7 @@ INSERT INTO `visitor_logs` (`id`, `user_id`, `ip_address`, `method`, `path`, `fu
 (8529, NULL, '3.224.234.70', 'GET', '/blog', 'https://sastrabhinnekakarya.co.id/blog', 200, 'Mozilla/5.0 (compatible)', NULL, '2026-07-09 06:22:27', '2026-07-09 06:22:27'),
 (8530, NULL, '23.23.253.54', 'GET', '/blog', 'https://sastrabhinnekakarya.co.id/blog', 200, 'Mozilla/5.0 (compatible)', NULL, '2026-07-09 06:28:05', '2026-07-09 06:28:05'),
 (8531, NULL, '3.224.234.70', 'GET', '/blog', 'https://sastrabhinnekakarya.co.id/blog', 200, 'Mozilla/5.0 (compatible)', NULL, '2026-07-09 06:29:30', '2026-07-09 06:29:30'),
-(8532, NULL, '3.224.234.70', 'GET', '/blog', 'https://sastrabhinnekakarya.co.id/blog', 200, 'Mozilla/5.0 (compatible)', NULL, '2026-07-09 06:42:37', '2026-07-09 06:42:37');
+(8532, NULL, '3.224.234.70', 'GET', '/blog', 'https://sastrabhinnekakarya.co.id/blog', 200, 'Mozilla/5.0 (compatible)', NULL, '2026-07-09 06:42:37', '2026-07-09 06:42:37'),
+(8533, NULL, '51.195.244.86', 'GET', '/blog/peran-persetujuan-lingkungan-amdal-dan-ukl-upl-dalam-pembangunan', 'https://sastrabhinnekakarya.co.id/blog/peran-persetujuan-lingkungan-amdal-dan-ukl-upl-dalam-pembangunan', 404, 'Mozilla/5.0 (compatible; AhrefsBot/7.0; +http://ahrefs.com/robot/)', NULL, '2026-07-09 07:31:57', '2026-07-09 07:31:57');
 
 SET FOREIGN_KEY_CHECKS = 1;
